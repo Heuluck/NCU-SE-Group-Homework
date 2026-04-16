05b61028ca737eb417bee25912bcfa293657f286
马骏 <1120393918@qq.com>
2026-04-16 23:32:04 +0800

    Revert "feat: 完成前端"

--- fe/app/components/ErrorToast.tsx
+++
@@ -1,8 +0,0 @@
-import { message } from "antd";
-
-export function showError(err: any) {
-  const msg = err?.message || err?.data?.message || "操作失败";
-  message.error(msg);
-}
-
-export default showError;
